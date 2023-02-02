1d23a943a410d1e4deda4029c137e7d57c6a1dc1
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-02-03 00:41:42 +0200

    Bumper slash commands

--- src/main/java/ua/com/company/App.java
+++ src/main/java/ua/com/company/App.java
@@ -2,15 +2,8 @@ package ua.com.company;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ua.com.company.listener.ContextMenuBot;
-import ua.com.company.listener.GuildJoinListener;
-import ua.com.company.listener.RoleCreateListener;
-import ua.com.company.listener.UserJoinListener;
 import ua.com.company.utils.PropertiesReader;
 
 public class App {
@@ -19,10 +12,11 @@ public class App {
         JDA jda = JDABuilder.createDefault(PropertiesReader.getToken())
                 .enableIntents(GatewayIntent.GUILD_MEMBERS,
                         GatewayIntent.MESSAGE_CONTENT)
-                .addEventListeners(new UserJoinListener())
-                .addEventListeners(new GuildJoinListener())
-                .addEventListeners(new RoleCreateListener())
-                .addEventListeners(new ContextMenuBot())
+//                .addEventListeners(new UserJoinListener())
+//                .addEventListeners(new GuildJoinListener())
+//                .addEventListeners(new RoleCreateListener())
+//                .addEventListeners(new ContextMenuBot())
+                .addEventListeners(new GuildJoin())
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .build();
 
---
+++ src/main/java/ua/com/company/GuildJoin.java
@@ -0,0 +1,25 @@
+package ua.com.company;
+
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public class GuildJoin extends ListenerAdapter {
+    /**
+     * https://jda.wiki/using-jda/interactions/#command-interactions
+     *Add slash commands
+     * @param event
+     */
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        event.getGuild().updateCommands().addCommands(
+
+                Commands.slash("bumper", "Notifications when it's time to BUMP!")
+                        .addSubcommands(new SubcommandData("add", "Add to bumper list."))
+                        .addSubcommands(new SubcommandData("list", "Show list of bumpers."))
+        ).queue();
+    }
+
+
+}
